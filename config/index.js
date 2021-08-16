e4de6005570b310d8363270ce05004d26cc3a2b1
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-16 23:37:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(812) 998-6356",
-  message: process.env.TEXTNOW_MESSAGE || "code 1919",
-  cookies_str: process.env.TEXTNOW_COOKIES || "connect.sid=s:GsApkUQN6-Zgd3tUhlsWL0DK58kz15d4.EaOZOIfTSchi6N9VNEI9+Gw8fJ1v9WHS4xTXQhG5Hh4; G_ENABLED_IDPS=google; pxcts=5265ff80-fea4-11eb-b299-0b997189c1f8; _pxvid=5265beee-fea4-11eb-b891-53476b4f4567; _px3=61d6066c74915af78dad92520e962d48cf444378ba41ea62f7afbbba196a31fb:76TPZmuxwn+NqeYci38yqD6tHSLzquGgIb6tQ5+IleBI9SmoIj0KYuxFgLMgmBN0X7iCi0G5eVibe+k1TFscdQ==:1000:KNyvLk+cBHUcqdeXpMYAMwDbzI/e1qIFkCajKNJBnIWPLmBwQjDa17BAZ26BCRgSCBFl7lQxT/LGNdWYn/xGxKn5qSUxcLXOcSrWwKNu1GojhM3GSlE8n14MzMtszoj/CZhXLVrPjC7B0ji3l9Rhf/FGJlcwSqvHKJdfZMy8aGUO+gvomZbrJ4yz4XpvXaMdFHeRAA03ipjK4tSYDWx8dw==; XSRF-TOKEN=8A3cMWbG-ctuocCjCv7vuGJGvkwkYzkdBQNQ"
+  recipient: process.env.TEXTNOW_RECIPIENT || "",
+  message: process.env.TEXTNOW_MESSAGE || "",
+  cookies_str: process.env.TEXTNOW_COOKIES || ""
 };
