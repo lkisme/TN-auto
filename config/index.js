e3d259a7b7f8422206a4222e198420db8f9994d6
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-17 00:12:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
   recipient: process.env.TEXTNOW_RECIPIENT || "",
-  message: process.env.TEXTNOW_MESSAGE || "",
+  //message: process.env.TEXTNOW_MESSAGE || "",
   cookies_str: process.env.TEXTNOW_COOKIES || ""
 };
