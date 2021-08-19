28316392ecfa20323756038978a8a0e33a958101
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-19 21:16:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ module.exports = {
   //password: process.env.TEXTNOW_PASSWORD || "",
   //recipient: process.env.TEXTNOW_RECIPIENT || "",
   //message: process.env.TEXTNOW_MESSAGE || "",
-  cookies_str: process.env.TEXTNOW_COOKIES || ""
+  cookies_str: process.env.TEXTNOW_COOKIES,
+  barkURL: process.env.BARK_URL;
 };
